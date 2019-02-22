1a7493e3f75a80570ce981e1a3f4ac086d528cbd
Beatriz Barroso <beatrizscnbarroso@gmail.com>
2019-02-22 12:10:32 +0000

    try 2

--- db/migrate/20190221122852_drop_doses_table.rb
+++
@@ -1,5 +0,0 @@
-class DropDosesTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :doses
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+file = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredients = JSON.parse(file)
+
+puts "Parsing the ingredients"
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+file_path = open('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail').read
+JSON.parse(file_path)["drinks"].each do |cocktail|
+  Cocktail.create(name: cocktail["strDrink"])
+end
+
+puts "Closing the ingredients"
+
+
